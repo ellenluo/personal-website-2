5fde3b440b6044688f3da2e00dbefbce24e5060b
Ellen Luo <ellenyilan.luo@gmail.com>
2022-07-04 23:44:59 -0700

    Setup and nav bar

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+import NavBar from './components/NavBar'
+import Router from './components/Router'
+
+const App = () => (
+  <React.Fragment>
+    <NavBar />
+    <Router />
+  </React.Fragment>
+)
+
+export default App
---
+++ web/src/components/Router.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Navigate, Routes, Route, Link } from 'react-router-dom'
+
+import { Main } from '../pages/Main'
+
+const Router = () => (
+  <Routes>
+    <Route element={Main} path="/home" />
+    <Route path="*" element={<Navigate to="/home" replace />} />
+  </Routes>
+)
+
+export default Router
