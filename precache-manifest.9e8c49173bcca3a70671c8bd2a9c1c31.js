4266914a0d63f28efa848fbf5aeea678820b9e6d
Ellen Luo <ellenyilan.luo@gmail.com>
2023-06-05 00:30:34 -0700

    Updates

---
+++ precache-manifest.9e8c49173bcca3a70671c8bd2a9c1c31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b278156a4927b043c1d",
+    "url": "/static/css/main.1a92e0c8.chunk.css"
+  },
+  {
+    "revision": "3b278156a4927b043c1d",
+    "url": "/static/js/main.f62ae4e4.chunk.js"
+  },
+  {
+    "revision": "4c789d2e5fabc298d51f",
+    "url": "/static/js/runtime~main.c246c43f.js"
+  },
+  {
+    "revision": "0cb70ec8f0cb9482e482",
+    "url": "/static/js/2.85e4cd7a.chunk.js"
+  },
+  {
+    "revision": "7131cb1ed12c9d329ffd",
+    "url": "/static/js/3.bba72b8e.chunk.js"
+  },
+  {
+    "revision": "7d755de363029fec04585de4d30e1adb",
+    "url": "/index.html"
+  }
+];
